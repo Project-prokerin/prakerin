4da5d0b504c6a770f67831b2b227bf8bfd6da13c
rafie.aydin1@gmail.com <rafie.aydin1@gmail.com>
2021-01-18 16:16:42 +0700

    prokerin
    Update sseeder

--- prokerin/database/seeders/UserSeeder.php
+++ prokerin/database/seeders/UserSeeder.php
@@ -21,19 +21,19 @@ class UserSeeder extends Seeder
         for ($i = 0; $i < 2; $i++) {
         DB::table('users')->insert([
             'name' => $faker->name,
-            'email' => $faker->name . '@gmail.com',
+            'email' => $faker->email,
             'password' => Hash::make('password'),
             'role' => 'siswa'
         ]);
         DB::table('users')->insert([
             'name' => $faker->name,
-            'email' => $faker->name . '@gmail.com',
+            'email' => $faker->email,
             'password' => Hash::make('password'),
             'role' => 'kaprok'
         ]);
         DB::table('users')->insert([
             'name' => $faker->name,
-            'email' => $faker->name . '@gmail.com',
+            'email' => $faker->email,
             'password' => Hash::make('password'),
             'role' => 'hubin'
         ]);
