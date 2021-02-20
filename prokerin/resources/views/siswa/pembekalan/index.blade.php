9c1e453af8cc199b44935f37b81add3fc0b5206d
Raditya Nugra <68360964+RadityaNugra@users.noreply.github.com>
2021-02-20 15:39:14 +0700

    summary
    pembekalan

--- prokerin/resources/views/siswa/pembekalan/index.blade.php
+++ prokerin/resources/views/siswa/pembekalan/index.blade.php
@@ -1,6 +1,24 @@
 @extends('template.master')
 @push('link')
+<style>
+/* .card{
+    width: 1000px;
+    height: 600px; */
+.header{
+     text-align:center;   
+}
+@media screen and (max-width:413px){
 
+.card{
+  width: auto;
+  float: none;
+}
+{
+  width: auto;
+  float: none;
+}
+}
+</style>
 @endpush
 @section('title', 'Prakerin | Pembekalan')
 @section('judul', 'PEMBEKALAN MAGANG')
@@ -8,7 +26,44 @@
         <div class="breadcrumb-item "><a href="{{ route('index.user') }}"><i class="fas fa-tachometer-alt"></i> DASBOARD</a></div>
         <div class="breadcrumb-item"> <i class="fas fa-newspaper"></i> PEMBEKALAN MAGANG</div>
 @endsection
-@section('content')
+@section('main')
+
+        <div class="card  ">
+<div class="header border-bottom border-dark">
+        
+        <H1>Pembekalan Magang</H1>
+      
+</div>
+<div class="row container mw-100 mx-auto ">
+<table class="table table-bordered  table-hover">
+  <thead>
+    <tr>
+      <th scope="col">Test</th>
+      <th scope="col">Status</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">Test WPT IQ</th>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="row">Test Personality interview</th>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="row">Test soft skill</th>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="row">File Portofolio</th>
+      <td><input type="file"></td>
+    </tr>
+  </tbody>
+</table>
+        </div>
+       
+        </div>
 
 @endsection
 @push('script')
