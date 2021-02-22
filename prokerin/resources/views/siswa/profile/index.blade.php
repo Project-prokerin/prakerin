dcf6f0d5f23da6a8f0b21143eb4e48caa52dae2e
waladahlmzaqi <68367660+waladahlmzaqi@users.noreply.github.com>
2021-02-22 11:07:49 +0700

    update siswa profile

--- prokerin/resources/views/siswa/profile/index.blade.php
+++ prokerin/resources/views/siswa/profile/index.blade.php
@@ -1,53 +1,19 @@
 @extends('template.master')
 @push('link')
 <style>
-    @media (max-width:432px) {
-
-.col-6.col-lg-6.border-right.border-dark.pt-4
-{
-  border:0px solid !important
-
-  }
+  *{
+    text-decoration: none;
+    }
 
 
-}
-/* .card{
-    width: 1200px;
-    height: 600px;
-}.pro{
-    position: absolute;
-    width: 53px;
-    height: 25px;
-    left: 40px;
-    top: 20px;
-}.icon{
-    position: absolute;
-    left: 40px;
-    top: 80px;
-}.kanan{
-    position: absolute;
-    width: 450px;
-    height: 45px;
-    right: 90px;
-    top: 80px;
-}.kanan2{
-    position: absolute;
-    width: 450px;
-    height: 45px;
-    right: 90px;
-    top: 400px;
-}.logout{
-    color: red;
-}.mail{
-    position: absolute;
-    width: 410px;
-    height: 45px;
-    right: 10px;
-    top: 20px;
-}.container{
-    width:1200px;
-    height:600px;
-} */
+
+      @media (max-width:432px) {
+        .col-6.col-lg-6.border-right.border-dark.pt-4
+      {
+        border:0px solid !important
+
+      }
+    }
 </style>
 @endpush
 @section('title', 'Prakerin | Profile')
@@ -60,47 +26,133 @@
 @section('main')
 <div class="card">
 <div class="card-header  border-bottom border-dark">Profile</div>
-
   <div class="row container  mx-3 ">
     <div class="col-6 col-lg-6 border-right border-dark pt-4 col-sm=12 ">
-     <div>
-       NIPD
-      <p> <i class="fas fa-user"></i> 000000000</div></p>
-
-      
-       Nama Siswa
-       <p> <i class="fas fa-user"></i> Nur Firdaus</p>
-
-       
-       Jurusan
-       <p> <i class="fas fa-cog"></i> Rekayasa Perangkat Lunak</p>
-
-       
-       kelas
-       <p> <i class="fas fa-user"></i> XI</p>
-
-       
-       No HP
-      <p> <i class="fas fa-user"></i> 090971386135</p>
+      <div>
+        <li class="media">
+          <a href="#">
+              <table border="1">
+                  <tr>
+                      <td style="height:30px; width:30px;"><i class="far fa-user" style="margin-left: 8px;"></i></td>
+                  </tr>
+              </table>
+          </a>
+          <div class="media-body ml-3" style="margin-top: -8px;">
+              <div class="media-title"><p>NIPD</p></div>
+              <div class="" style="margin-top: -14px;"><h6>000000000</h6></div>
+          </div>
+        </li>
+        <li class="media mt-3">
+          <a href="#">
+              <table border="1">
+                  <tr>
+                      <td style="height:30px; width:30px;"><i class="far fa-user" style="margin-left: 8px;"></i></td>
+                  </tr>
+              </table>
+          </a>
+          <div class="media-body ml-3" style="margin-top: -8px;">
+              <div class="media-title"><p>Nama Siswa</p></div>
+              <div class="" style="margin-top: -14px;"><h6>Nur Firdaus</h6></div>
+          </div>
+        </li>
+        <li class="media mt-3">
+          <a href="#">
+              <table border="1">
+                  <tr>
+                      <td style="height:30px; width:30px;"><i class="fas fa-cog" style="margin-left: 8px;"></i></td>
+                  </tr>
+              </table>
+          </a>
+          <div class="media-body ml-3" style="margin-top: -8px;">
+              <div class="media-title"><p>Jurusan</p></div>
+              <div class="" style="margin-top: -14px;"><h6>Rekayasa Perangkat Lunak</h6></div>
+          </div>
+        </li>
+        <li class="media mt-3">
+          <a href="#">
+              <table border="1">
+                  <tr>
+                      <td style="height:30px; width:30px;"><i class="far fa-user" style="margin-left: 8px;"></i></td>
+                  </tr>
+              </table>
+          </a>
+          <div class="media-body ml-3" style="margin-top: -8px;">
+              <div class="media-title"><p>Kelas</p></div>
+              <div class="" style="margin-top: -14px;"><h6>XI</h6></div>
+          </div>
+        </li>
+        <li class="media mt-3 mb-3">
+          <a href="#">
+              <table border="1">
+                  <tr>
+                      <td style="height:30px; width:30px;"><i class="far fa-user" style="margin-left: 8px;"></i></td>
+                  </tr>
+              </table>
+          </a>
+          <div class="media-body ml-3" style="margin-top: -8px;">
+              <div class="media-title"><p>No HP</p></div>
+              <div class="" style="margin-top: -14px;"><h6>083896802804</h6></div>
+          </div>
+        </li>
+      </div>
     </div>
 
-
-    
     <div class="col pl-5 lg-6 ">
-    
-      
       <br>
-      Email
-      <p><i class="fas fa-envelope-open"></i> nurfirdaus@gmail.com</p>
-    <br><br><br>
-      <p><b>User Profile</b></p>
-      <p>Edit Profile</p>
-      <p>Change Password</p>
-      <p>Log Out</p>
-      <p></p>
+      <div>
+        <li class="media mb-3">
+          <a href="#">
+              <table border="1">
+                  <tr>
+                      <td style="height:30px; width:30px;"><i class="far fa-user" style="margin-left: 8px;"></i></td>
+                  </tr>
+              </table>
+          </a>
+          <div class="media-body ml-3" style="margin-top: -8px;">
+              <div class="media-title"><p>Email</p></div>
+              <div class="" style="margin-top: -14px;"><h6>nurfirdaus@gmail.com</h6></div>
+          </div>
+        </li>
+      </div>
+      <br><br><br>
+      <div>
+        <li class="media">
+          <a href="#">
+              <table border="1">
+                  <tr>
+                      <td style="height:30px; width:30px;"><i class="far fa-user" style="margin-left: 8px;"></i></td>
+                  </tr>
+              </table>
+          </a>
+          <div class="media-body ml-3" style="margin-top: 4px;">
+              <div class="media-title"><p>User Profile</p></div>
+          </div>
+        </li>
+        <li class="media" style="margin-top: -15px; margin-left: 32px;">
+          <a href="#">
+            <div class="media-body ml-3" style="margin-top: 4px;">
+              <div class="media-title"><p>Edit Profile</p></div>
+            </div>
+          </a>
+        </li>
+        <li class="media" style="margin-top: -15px; margin-left: 32px;">
+          <a href="#">
+            <div class="media-body ml-3">
+              <div class="media-title"><p>Change Password</p></div>
+            </div>
+          </a>
+        </li>
+        <li class="media" style="margin-top: -15px; margin-left: 32px;">
+          <a href="#">
+            <div class="media-body ml-3">
+              <div class="media-title"><p>Log Out</p></div>
+            </div>
+          </a>
+        </li>
+      </div>
     </div>
-  
  </div>
+</div>
 @endsection
 @push('script')
 
