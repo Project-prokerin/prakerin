727432cb8f994b7daf33aace83c5609552028392
Raditya Nugra <68360964+RadityaNugra@users.noreply.github.com>
2021-02-22 10:39:51 +0700

    status prakerin

--- prokerin/resources/views/siswa/status/index.blade.php
+++ prokerin/resources/views/siswa/status/index.blade.php
@@ -1,6 +1,10 @@
 @extends('template.master')
 @push('link')
-
+<style>
+        .card{
+               
+        }
+</style>
 @endpush
 @section('title', 'Prakerin | Status Magang')
 @section('judul', 'STATUS MAGANG')
@@ -9,7 +13,43 @@
         <div class="breadcrumb-item"> <i class="fas fa-newspaper"></i> STATUS MAGANG</div>
 @endsection
 @section('main')
+<div class="card">
 
+<div class="container pt-4 pb-4">
+<table class="table table-bordered  table-striped table-hover ">
+  <thead> 
+   
+  </thead>
+  <tbody>
+  <tr>
+      <th scope="col">Nama</th>
+      <th scope="col">-</th>
+    </tr>
+    <tr>
+      <th scope="col">Status</th>
+      <th scope="col">-</th>
+    </tr>
+    <tr>
+      <th scope="col">Tanggal Mulai</th>
+      <th scope="col">-</th>
+    </tr>
+    <tr>
+      <th scope="col">Tanggal Selesai</th>
+      <th scope="col">-</th>
+    </tr>
+    <tr>
+      <th scope="col">Nama Perusahaan</th>
+      <th scope="col">-</th>
+    </tr>
+    <tr>
+      <th scope="col">Lokasi Perusahaan</th>
+      <th scope="col">-</th>
+    </tr>
+  </tbody>
+</table>
+</div>
+
+</div>
 @endsection
 @push('script')
 
