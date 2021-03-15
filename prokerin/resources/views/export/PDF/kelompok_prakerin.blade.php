a4cc335838b9a5b70d6b5523b009761e5518d5dd
Raditya Nugra <68360964+RadityaNugra@users.noreply.github.com>
2021-03-15 14:40:22 +0700

    ini pdf uodate

--- prokerin/resources/views/export/PDF/kelompok_prakerin.blade.php
+++ prokerin/resources/views/export/PDF/kelompok_prakerin.blade.php
@@ -19,6 +19,7 @@
     }
     .font1{
         font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif
+        
     }
     .font2{
         font-family: 'Ribeye', cursive;
@@ -26,6 +27,72 @@
     .font3{
         font-family: sans-serif
     }
+
+    h1{
+  font-family: sans-serif;
+}
+ 
+table {
+  font-family: Arial, Helvetica, sans-serif;
+  color: #666;
+  text-shadow: 1px 1px 0px #fff;
+  background: #eaebec;
+  border: #ccc 1px solid;
+}
+ 
+table th {
+  padding: 15px 35px;
+  border-left:1px solid #e0e0e0;
+  border-bottom: 1px solid #e0e0e0;
+  background: #ededed;
+}
+ 
+table th:first-child{  
+  border-left:none;  
+}
+ 
+table tr {
+  text-align: center;
+  padding-left: 20px;
+}
+ 
+table td:first-child {
+  text-align: left;
+  padding-left: 20px;
+  border-left: 0;
+}
+ 
+table td {
+  padding: 15px 35px;
+  border-top: 1px solid #ffffff;
+  border-bottom: 1px solid #e0e0e0;
+  border-left: 1px solid #e0e0e0;
+  background: #fafafa;
+  background: -webkit-gradient(linear, left top, left bottom, from(#fbfbfb), to(#fafafa));
+  background: -moz-linear-gradient(top, #fbfbfb, #fafafa);
+}
+ 
+table tr:last-child td {
+  border-bottom: 0;
+}
+ 
+table tr:last-child td:first-child {
+  -moz-border-radius-bottomleft: 3px;
+  -webkit-border-bottom-left-radius: 3px;
+  border-bottom-left-radius: 3px;
+}
+ 
+table tr:last-child td:last-child {
+  -moz-border-radius-bottomright: 3px;
+  -webkit-border-bottom-right-radius: 3px;
+  border-bottom-right-radius: 3px;
+}
+ 
+table tr:hover td {
+  background: #f2f2f2;
+  background: -webkit-gradient(linear, left top, left bottom, from(#f2f2f2), to(#f0f0f0));
+  background: -moz-linear-gradient(top, #f2f2f2, #f0f0f0);
+}
 </style>
 <body>
     <div >
@@ -35,22 +102,25 @@
             </div>
             <div >
                 <div >
-                    <h4 class="font1"><b>YAYASAN SETYA BHAKTI</b></h4>
+                <center>
+                    <h4 class="font1 text-center"><b>YAYASAN SETYA BHAKTI</b></h4>
                     <h1 class="font2">SMK TARUNA BHAKTI</h1>
                     <h6 class="font3"><b>TERAKREDITASI: "A" No: 02.00/203/BAN-SM/XII/2018</b></h6>
                     <h6 class="font3"><small><b>Izin No: 421.4/1150/DISDIK/2004 - NPSN : 20229232 </b></small></h6>
                     <h6 class="font3"> <small> KOMPENTENSI KEAHLIAN : </small></h6>
                     <h6 class="font3"> <small> PRODUKSI DAN SIARAN PROGRAM TELEVISI | TEKNIK ELEKTRONIKA INDUSTRI </small></h6>
                     <h6 class="font3"> <small> TEKNIK KOMPUTER DAN JARINGAN | MULTIMEDIA | REKAYASA PERANGKAT LUNAK</small></h6>
+                    <h6 class="font3"><small>Jln Pekapuran Kel. Curug-Kec.Cimanggis Depok Kode Pos 16953 Telp (021) 8744810</small> </h6>
+                    <h6 class="font3"> <small><b>Website</b><a href="http://www.smktarunabhakti.net"> http://www.smktarunabhakti.net </a> / <b>E-mail: taruna@smktarunabhakti.net</b> </small> </h6>
                 </div>
-        
+        </center>
             </div>
          
             
         </div>
         
         
-        <table border="1">
+        <table cellspacing='0'>
         <thead>
             <tr>
               <th>NO</th>
