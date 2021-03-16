94124963ccaa96f015189a3bb09bcbf467fe1252
NurFirdausR <nurfirdaus.5000@gmail.com>
2021-03-16 17:13:10 +0700

    Surat PDF

--- prokerin/app/Http/Controllers/PDF/PDFController.php
+++ prokerin/app/Http/Controllers/PDF/PDFController.php
@@ -10,6 +10,9 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\kelompok_laporan;
 // export pdf
 use PDF;
+use Carbon\Carbon;
+
+
 
 class PDFController extends Controller
 {
@@ -27,9 +30,25 @@ class PDFController extends Controller
         $no_kelompok = !empty(Auth::user()->siswa->data_prakerin->kelompok_laporan->no) ? Auth::user()->siswa->data_prakerin->kelompok_laporan->no : '';
         $guru_nama = !empty(Auth::user()->siswa->data_prakerin->kelompok_laporan->guru->nama) ? Auth::user()->siswa->data_prakerin->kelompok_laporan->guru->nama : '';
         
+
+        // $options = new Options();
+        // $options->setIsRemoteEnabled(true);
+        // $dompdf = new Dompdf($options);
+        // $dompdf->loadHtml($html);
+        // $dompdf->output();
+    //     $data = Employee::all();
+
+    //   // share data to view
+    //   view()->share('employee',$data);
+    //   $pdf = PDF::loadView('pdf_view', $data);
+
+    //   // download PDF file with download method
+    //   return $pdf->download('pdf_file.pdf')
+    $waktu = Carbon::now()->isoFormat('D MMMM Y');
         $kelompok = kelompok_laporan::has('data_prakerin')->where('no', $no_kelompok)->get();
-            $KPrakerin = PDF::loadView('export.PDF.kelompok_prakerin', compact('kelompok'))->setOptions(['defaultFont' => 'sans-serif']); ;
-            return $KPrakerin->download('KelompokPrakerin.PDF');
+            $KPrakerin = PDF::loadView('export.PDF.kelompok_prakerin', compact('kelompok','waktu'))->setOptions(['defaultFont' => 'sans-serif','isHtml5ParserEnabled' => true, 'isRemoteEnabled' => true]); 
+            // return $KPrakerin->download('KelompokPrakerin.PDF');
+            return $KPrakerin->stream();
         // return view('export.PDF.kelompok_prakerin',compact('kelompok'));
     }
     
