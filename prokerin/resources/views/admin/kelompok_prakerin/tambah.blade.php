ab15837471fa40b37d8412cdf2065bfc98b91422
Raditya Nugra <68360964+RadityaNugra@users.noreply.github.com>
2021-06-14 12:02:24 +0700

    input

--- prokerin/resources/views/admin/kelompok_prakerin/tambah.blade.php
+++ prokerin/resources/views/admin/kelompok_prakerin/tambah.blade.php
@@ -9,16 +9,16 @@
         }
 
         .card {
-            height: 600px;
+            height: auto;
         }
 
         .kanan {
             margin-left: 40x;
         }
 
-        .buton {
+        /* .buton {
             margin-left: 900px;
-        }
+        } */
 
     </style>
 @endpush
@@ -30,22 +30,22 @@
     <div class="breadcrumb-item"> <i class="far fa-building"></i> DATA PERUSAHAAN</div>
 @endsection
 @section('main')
-    <div class="card mt-5" style="height: 700px;">
-        <div class="container text-center mt-5 mb-3 ml-1">
-            <h3>Tambah kelompok</h3>
+<div class="row">
+    <div class="col-12">
+      <div class="card">
+        <div class="card-header">
+          <h4>Tambah Kelompok</h4>
         </div>
+        <div class="card-body">
 
-        <div class="container">
+        
             <form action="{{ route('kelompok.post') }}" method="POST">
                 @csrf
 
-                <div class="row mt-3 ml-4 ">
-                    <div class="col-6  kanan">
-                        <!-- no kelom -->
-                        <div class="form-group col-lg-10">
-                            <label>No Kelompok</label>
+                <div class="input-group ">
+                        <div class="col-3">
                             <select class="form-control select2 @error('no')  is-invalid  @enderror" name="no" id="">
-                                <option value="">Pilih Nomor</option>
+                                <option value="">--Pilih Nomor--</option>
                                 <option value="1" @if (old('no') === '1') selected @endif>1</option>
                                 <option value="2" @if (old('no') === '2') selected @endif>2</option>
                                 <option value="3" @if (old('no') === '3') selected @endif>3</option>
@@ -56,11 +56,7 @@
                                 <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
                         </div>
-
-
-                        <!-- gru bimbing -->
-                        <div class="form-group col-lg-10 ">
-                            <label>Guru Pembimbing</label>
+                         <div class="col-3 ">
                             <select class="form-control select2 @error('id_guru')  is-invalid  @enderror" name="id_guru"
                                 id="">
                                 <option value="">--Cari Guru--</option>
@@ -73,24 +69,7 @@
                                 <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
                         </div>
-                        <!-- jurusan -->
-                        {{-- <div class="form-group col-lg-10 ">
-                            <label>Jurusan</label>
-                            <select name="jurusan" class="form-control select2 @error('jurusan')  is-invalid  @enderror"  name=" jurusan" id="">
-                                <option value="" selected>--Pilih Jurusan--</option>
-                                <option value="RPL">Rekayasa Perangkat Lunak</option>
-                                <option value="TKJ">Teknik Komunikasi Jaringan</option>
-                                <option value="BC">Broadcasting</option>
-                                <option value="MM">Multimedia</option>
-                                <option value="TEI">TeknikElektronikaIndustri</option>
-                            </select>
-                                @error('jurusan')
-                                    <div class="invalid-feedback">{{ $message }}</div>
-                                @enderror
-                        </div> --}}
-                        <!-- perusahaan -->
-                        <div class="form-group col-lg-10 ">
-                            <label>Perusahaan</label>
+                        <div class="col-3">
                             <select name="id_perusahaan"
                                 class="form-control   @error('id_perusahaan')  is-invalid  @enderror select2">
                                 <option value="">--Cari Perusahaan--</option>
@@ -102,76 +81,18 @@
                                 <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
                         </div>
-
-
-                        <div class="form-group col-lg-10 ">
-                            <label for="">No telepon</label>
+                        <div class="col-3">
                             <input class="form-control @error('no_telpon')  is-invalid  @enderror" type="number"
                                 name="no_telpon" placeholder="no tlp" aria-label="default input example">
                             @error('no_telpon')
                                 <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
                         </div>
+</div>
 
 
-
-                    </div>
-                    <div class="col-6">
-                        <label>Daftar Nama Siswa</label>
-
-
-                        <div class="form-group col-lg-12 ">
-                            <select name="id_data_prakerin[]"
-                                class="form-control  @error('id_data_prakerin')  is-invalid  @enderror select2">
-                                <option value="">--Cari Siswa--</option>
-                                @forelse ($data_prakerin as $item)
-                                    @if (empty($item->kelompok_laporan))
-                                        <option value="{{ $item->id }}">{{ $item->nama }}</option>
-                                    @endif
-                                @empty
-                                    <option disabled>Semua Siswa telah mendapat kelompok!</option>
-                                @endforelse
-
-
-
-                            </select>
-                            @if ($errors->has(`id_data_prakerin.0`))
-                                <span class="text-danger">
-                                    <small> 
-                                        {{ $errors->first('id_data_prakerin.0') }}
-                                    </small>
-
-                                </span>
-
-                            @endif
-
-                        </div>
-
-                        <div class="form-group col-lg-12 ">
-                            <select name="id_data_prakerin[]"
-                                class="form-control  @error('id_data_prakerin')  is-invalid  @enderror select2">
-                                <option value="">--Cari Siswa--</option>
-                                @forelse ($data_prakerin as $item)
-                                    @if (empty($item->kelompok_laporan))
-                                        <option value="{{ $item->id }}">{{ $item->nama }}</option>
-                                    @endif
-                                @empty
-                                    <option disabled>Semua Siswa telah mendapat kelompok!</option>
-                                @endforelse
-                            </select>
-                            @if ($errors->has(`id_data_prakerin.1`))
-                                <span class="text-danger">
-                                    <small>
-                                        {{ $errors->first('id_data_prakerin.1') }}
-                                    </small>
-
-                                </span>
-                            @endif
-
-                        </div>
-
-
-                        <div class="form-group col-lg-12 ">
+                 <div class="input-group" style="margin-top:20px;">
+                        <div class="col-3">
                             <select name="id_data_prakerin[]"
                                 class="form-control  @error('id_data_prakerin')  is-invalid  @enderror select2">
                                 <option value="">--Cari Siswa--</option>
@@ -193,9 +114,7 @@
                             @endif
 
                         </div>
-
-
-                        <div class="form-group col-lg-12 ">
+                        <div class="col-3">
                             <select name="id_data_prakerin[]"
                                 class="form-control  @error('id_data_prakerin')  is-invalid  @enderror select2">
                                 <option value="">--Cari Siswa--</option>
@@ -207,27 +126,75 @@
                                     <option disabled>Semua Siswa telah mendapat kelompok!</option>
                                 @endforelse
                             </select>
-                            @if ($errors->has(`id_data_prakerin.3`))
+                            @if ($errors->has(`id_data_prakerin.2`))
                                 <span class="text-danger">
                                     <small>
-                                        {{ $errors->first('id_data_prakerin.3') }}
+                                        {{ $errors->first('id_data_prakerin.2') }}
                                     </small>
 
                                 </span>
                             @endif
 
                         </div>
+                        <div class="col-3">
+                            <select name="id_data_prakerin[]"
+                                class="form-control  @error('id_data_prakerin')  is-invalid  @enderror select2">
+                                <option value="">--Cari Siswa--</option>
+                                @forelse ($data_prakerin as $item)
+                                    @if (empty($item->kelompok_laporan))
+                                        <option value="{{ $item->id }}">{{ $item->nama }}</option>
+                                    @endif
+                                @empty
+                                    <option disabled>Semua Siswa telah mendapat kelompok!</option>
+                                @endforelse
+                            </select>
+                            @if ($errors->has(`id_data_prakerin.2`))
+                                <span class="text-danger">
+                                    <small>
+                                        {{ $errors->first('id_data_prakerin.2') }}
+                                    </small>
 
-                        <button type="submit" class="btn btn-success ml-3"><i class="fas fa-check"></i> submit</button>
-                        <a href="{{ route('kelompok.index') }}" type="submit" class="btn btn-danger"><i
+                                </span>
+                            @endif
+
+                        </div>
+                        <div class="col-3">
+                            <select name="id_data_prakerin[]"
+                                class="form-control  @error('id_data_prakerin')  is-invalid  @enderror select2">
+                                <option value="">--Cari Siswa--</option>
+                                @forelse ($data_prakerin as $item)
+                                    @if (empty($item->kelompok_laporan))
+                                        <option value="{{ $item->id }}">{{ $item->nama }}</option>
+                                    @endif
+                                @empty
+                                    <option disabled>Semua Siswa telah mendapat kelompok!</option>
+                                @endforelse
+                            </select>
+                            @if ($errors->has(`id_data_prakerin.2`))
+                                <span class="text-danger">
+                                    <small>
+                                        {{ $errors->first('id_data_prakerin.2') }}
+                                    </small>
+
+                                </span>
+                            @endif
+
+                        </div>
+                    </div>
+
+               
+
+                        <button type="submit" class="btn btn-success ml-3" style="margin-top:20px;"><i class="fas fa-check"></i> submit</button>
+                        <a href="{{ route('kelompok.index') }}" type="submit" class="btn btn-danger"style="margin-top:20px;"><i
                                 class="fas fa-times"></i> Cancel</a>
 
                     </div>
 
                 </div>
+
+
             </form>
 
-        </div>
 
 
 
