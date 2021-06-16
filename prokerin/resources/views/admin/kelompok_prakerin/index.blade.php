82e0643db588effaceab7addc61162160aacfeea
Raditya Nugra <68360964+RadityaNugra@users.noreply.github.com>
2021-06-16 15:49:30 +0700

    update icon + input

--- prokerin/resources/views/admin/kelompok_prakerin/index.blade.php
+++ prokerin/resources/views/admin/kelompok_prakerin/index.blade.php
@@ -34,7 +34,7 @@
             <div class="col-12">
               <div class="card">
                 <div class="card-header">
-                  <h4>Data Guru</h4>
+                  <h4>Kelompok Prakerin</h4>
                 </div>
                 <div class="card-body">
 
