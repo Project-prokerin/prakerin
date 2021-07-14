96f2fc1b4e70cb066fd5375585fbf44daf4b685a
waladahlmzaqi <walada204@gmail.com>
2021-07-14 18:25:08 +0700

    update tambah tamatan + validasi

--- prokerin/resources/views/admin/penelusuran_tamatan/tambah.blade.php
+++ prokerin/resources/views/admin/penelusuran_tamatan/tambah.blade.php
@@ -4,8 +4,8 @@
 
 </style>
 @endpush
-@section('title','Prakerin | Tamatan Penelusuran')
-@section('judul','Tamatan Penelusuran')
+@section('title','Prakerin | Tambah Tamatan Penelusuran')
+@section('judul','Tambah Data Tamatan Penelusuran')
 @section('breadcrump')
 <div class="breadcrumb-item "><a href="{{ route('admin.dashboard') }}"><i class="fas fa-tachometer-alt"></i> DASBOARD</a></div>
 <div class="breadcrumb-item"> <i class="fas fa-user"></i> TAMATAN PENELUSURAN</div>
@@ -13,17 +13,13 @@
 @section('main')
 
 <div class="card" style="height: 600px;">
+    <div class="card-header mt-2">
+        <h4 class="pt-2"><i class="fas fa-align-justify mr-2"></i>Tambah Data Tamatan Penelusuran</h4>
+    </div>
     <div class="card-body">
         <div class="">
             <div class="col-lg-12" style="height: 500px;">
                 <div class="">
-                    <div class="">
-                        <div class="container mt-2">
-                            <h5 class="card-title">Tamatan Penelusuran</h5>
-                        </div>
-                        <hr>
-                    </div>
-
                     <div class="container">
                         <form action="" method="POST">
                                 @csrf
@@ -32,20 +28,28 @@
                                 <div class="col-6">
                                     <div class="mb-3 col-lg-10">
                                         <label>Nama Siswa Alumni</label>
-                                        <select name="" class="form-control   @error('')  is-invalid  @enderror select2">
+                                        <select name="" id="namasiswaalumni" class="form-control   @error('')  is-invalid  @enderror select2">
                                             <option value="" >--Cari Siswa Alumni--</option>
                                             {{-- @foreach ($ as $)
                                             <option value="{{$->id}}"{{(old('') == $->id) ? 'selected' : ''}}>{{$->}}</option>
                                             @endforeach --}}
                                         </select>
-                                            {{-- @error('')
-                                                <div class="invalid-feedback">{{ $message }}</div>
-                                            @enderror --}}
+                                        <div id="invalid_siswa" class="invalid-feedback d-none"></div>
+                                    </div>
+                                    <div class="mb-3 col-lg-10">
+                                        <label>Kelas</label>
+                                        <input type="text" class="form-control" disabled>
+                                        <div id="invalid_siswa" class="invalid-feedback d-none"></div>
+                                    </div>
+                                    <div class="mb-3 col-lg-10">
+                                        <label>Jurusan</label>
+                                        <input type="text" class="form-control" disabled>
+                                        <div id="invalid_siswa" class="invalid-feedback d-none"></div>
                                     </div>
                                     <div class="mb-3 col-lg-10">
                                         <label>Status</label>
-                                        <select id="status" name="" class="form-control   @error('')  is-invalid  @enderror select2">
-                                            <option value="#" >--Cari Status--</option>
+                                        <select id="status" name="" class="status form-control   @error('')  is-invalid  @enderror select2">
+                                            <option value="" >--Cari Status--</option>
                                             <option value="bekerja" >Bekerja</option>
                                             <option value="kuliah" >Kuliah</option>
                                             <option value="wirausaha" >Wirausaha</option>
@@ -55,9 +59,7 @@
                                             <option value="{{$->id}}"{{(old('') == $->id) ? 'selected' : ''}}>{{$->nama}}</option>
                                             @endforeach --}}
                                         </select>
-                                            {{-- @error('')
-                                                <div class="invalid-feedback">{{ $message }}</div>
-                                            @enderror --}}
+                                        <div id="invalid_status" class="invalid-feedback d-none"></div>
                                     </div>
                                 </div>
 
@@ -67,10 +69,8 @@
                                         <div class="mb-3 col-lg-10" id="namaperusahaan" hidden>
                                             <label>Nama Perusahaan</label>
                                             <div class="mb-3">
-                                                <input type="text" name="" id="" class="form-control @error('')  is-invalid  @enderror form-control" value="{{ old('') }}" >
-                                                @error('')
-                                                    <div class="invalid-feedback">{{ $message }}</div>
-                                                @enderror
+                                                <input type="text" name="" id="namaperusahaan" class="form-control @error('')  is-invalid  @enderror form-control" value="" >
+                                                <div id="invalid_namaperusahaan" class="invalid-feedback d-none"></div>
                                             </div>
                                         </div>
                                         <div class="mb-3 col-lg-10" id="alamatperusahaan" hidden>
@@ -146,7 +146,7 @@
 
                                     <div class="row">
                                         <a class="btn btn-danger ml-auto mr-3 text-white mt-3 mb-3" id="kembali">kembali </a>
-                                        <button type="submit"   class="submit mr-3 btn btn-success mt-3 mb-3"><i class="fas fa-check"></i> Submit</button>
+                                        <a type="submit" id="cek_submit" class="text-white mr-3 btn btn-success mt-3 mb-3"><i class="fas fa-check"></i> Submit</a>
                                     </div>
                                 </div>
                             </div>
@@ -226,6 +226,30 @@
                     $('#namausaha').prop('hidden', false);
                 }
             });
+    });
+
+    $('#cek_submit').on('click', function (params) {
+        var nama = $('#namasiswaalumni').val();
+        var status = $('#status').val();
+
+        if (nama == '' || status == '')  {
+            // option
+            if (nama == '') {
+            $('#namasiswaalumni').addClass('is-invalid');
+            $('#invalid_siswa').html('nama siswa alumni tidak boleh kosong').removeClass('d-none');
+            }else{
+                $('#invalid_siswa').addClass('d-none');
+            }
+
+            if (status == '') {
+            $('#status').addClass('is-invalid');
+            $('#invalid_status').html('status siswa alumni tidak boleh kosong').removeClass('d-none');
+            }else{
+                $('#invalid_status').addClass('d-none');
+            }
+        }
+        else{
+        }
     });
 </script>
 <script src="{{ asset('assets/js/pages-admin/.js') }}" ></script>
