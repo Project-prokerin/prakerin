1e48b25ddec6bec66b5980fbf22da628f5ebe469
Raditya Nugra <68360964+RadityaNugra@users.noreply.github.com>
2021-08-11 11:57:02 +0700

    solv template surat

--- prokerin/resources/views/export/PDF/kelompok_prakerin.blade.php
+++ prokerin/resources/views/export/PDF/kelompok_prakerin.blade.php
@@ -247,7 +247,7 @@
                         </p>
                     </div>
                     <div class="ttd">
-                        <h6 class="font6" style="margin-right: 52px;" >Depok, {{ $waktu }}</h6>
+                        <h6 class="font6" style="margin-right: 27px;" >Depok, {{ $waktu }}</h6>
                         <h6 class="font6" style="margin-top: -30px;">Kepala SMK Taruna Bhakti</h6>
                     </div>
 
@@ -262,7 +262,7 @@
                         </ol>
                     </div>
                     <div style="text-align: right;">
-                        <h6 style="font-size: 14px; margin-top: 80px;margin-right:30px;">Ramadin Tarigan, ST</h6>
+                        <h6 style="font-size: 14px; margin-top: 120px;margin-right:30px;">Ramadin Tarigan, ST</h6>
                         <h6 style="font-size: 14px; font-weight: light; margin-top: -30px;margin-right:10px;">NIK. 19760329200411101</h6>
                     </div>
 
